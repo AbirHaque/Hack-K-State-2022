c4f4a3e828beb9f20b69357e3b9148850415b031
shaddev <shadahmed2015@gmail.com>
2022-11-13 06:15:18 -0600

    update mobile app

---
+++ MusicProject/SliderTrackPlayer.js
@@ -0,0 +1,11 @@
+import Slider from '@react-native-community/slider';
+import TrackPlayer, {usePlaybackState} from 'react-native-track-player';
+import {useTrackPlayerProgress} from 'react-native-track-player/lib/hooks';
+
+const SliderTrackPlayer = () => {
+    return(
+        <></>
+    )
+}
+
+export default SliderTrackPlayer;
