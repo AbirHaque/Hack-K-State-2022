42f3c1bcd0379890b1538d90495a7c8cf336d4f5
Abir Haque <abir.haque.usa@gmail.com>
2022-11-12 21:04:10 -0600

    Multithreading done

--- daw/daw.py
+++ daw/daw.py
@@ -5,6 +5,9 @@ import simpleaudio as sa
 from scipy.io.wavfile import write
 import threading
 import requests
+from appwrite.client import Client
+from appwrite.input_file import InputFile
+from appwrite.services.storage import Storage
 
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
@@ -26,6 +29,7 @@ class GridWindow(Gtk.Window):
         self.num_pitches=13
         self.num_columns=4
         self.sample_rate=44100
+        self.samples_per_note=11025#44100/4
         self.note_frequency_mapping=[
             ('C4','261.63'),
             ('C#4',' 277.18'),
@@ -42,6 +46,10 @@ class GridWindow(Gtk.Window):
             ('C5','523.25'),
         ]
         self.note_frequency_mapping.reverse()
+        self.thread_results=[0,0,0,0]
+
+
+
         self.pitch_entries = [Gtk.Entry(text=str(self.note_frequency_mapping[x][0]),editable=False) for x in range (self.num_pitches)]
         self.music_matrix=[[Gtk.Entry() for y in range(self.num_columns)] for x in range (self.num_pitches)]
         
@@ -116,43 +124,60 @@ class GridWindow(Gtk.Window):
             return True
         except:
             return False
-    def gen_wav_binary(self):
-        sample_rate=44100
-        num_notes=len(self.music_matrix[0])
-        samples_per_note=11025#44100/4
-        num_samples=num_notes*samples_per_note
-        num_rows=len(self.music_matrix)
-        num_chunks=int((num_samples/sample_rate)+((num_samples%sample_rate)!=0))#Ceiling division
-        equation=self.instrument_combo.get_child().get_text()
+    def signal_computation_thread(self,id,buf,queue):
         parser=Parser()
-
-        final_audio=[0 for x in range(num_samples)] 
-
-
-        buf=[[0 for y in range(len(self.music_matrix[0]))] for x in range(len(self.music_matrix))]
-        for i in range(len(self.music_matrix)):
-            for j in range(len(self.music_matrix[i])):
-                #if j >=1 and self.music_matrix[i][j-1].get_text()!="" and self.music_matrix[i][j].get_text()=="" :
-                #    buf[i][j]=buf[i][j-1]#float(self.music_matrix[i][j-1].get_text())
-                #else:
-                #    buf[i][j]=0 if self.music_matrix[i][j].get_text()=="" else float(self.music_matrix[i][j].get_text())
-                buf[i][j]=0 if self.music_matrix[i][j].get_text()=="" else float(self.music_matrix[i][j].get_text())
-        for x in buf:
-            for y in x:
-                print(y,end=", ")
-            print("")
-        queue = parser.shunting_yard(equation)
-        print(queue)
-        print(num_samples)
+        final_audio=[0 for x in range(len(buf[0])*self.samples_per_note)] 
         for j in range(len(buf[0])):
             for i in range(len(buf)):
-                for t in range(0+j*samples_per_note,samples_per_note+j*samples_per_note):
+                for t in range(0+j*self.samples_per_note,self.samples_per_note+j*self.samples_per_note):
                     tmp=[token if token!="f" else self.note_frequency_mapping[i][1] for token in queue]#self.note_frequency_mapping[i][1] for token in queue]
                     tmp=[(token if token!='t' else str(t)) for token in tmp]
                     tmp=[(token if token!="a" else str(buf[i][j])) for token in tmp]                    
                     final_audio[t]+=parser.calculate(tmp)
-                
+        self.thread_results[id]= final_audio
+
+
+    def gen_wav_binary(self):
+        num_notes=len(self.music_matrix[0])
+        num_samples=num_notes*self.samples_per_note
+        num_rows=len(self.music_matrix)
+        num_chunks=int((num_samples/self.sample_rate)+((num_samples%self.sample_rate)!=0))#Ceiling division
+        equation=self.instrument_combo.get_child().get_text()
+        parser=Parser()
+
+        final_audio=[]#[0 for x in range(num_samples)] 
+
+
+        buf=np.array([[0 for y in range(len(self.music_matrix[0]))] for x in range(len(self.music_matrix))])
+        for i in range(len(self.music_matrix)):
+            for j in range(len(self.music_matrix[i])):
+                buf[i][j]=0 if self.music_matrix[i][j].get_text()=="" else float(self.music_matrix[i][j].get_text())
+        queue = parser.shunting_yard(equation)
+        partitions=np.hsplit(buf, 4)
+        t1 = threading.Thread(target=self.signal_computation_thread,args=(0,partitions[0],queue))
+        t2 = threading.Thread(target=self.signal_computation_thread,args=(1,partitions[1],queue))
+        t3 = threading.Thread(target=self.signal_computation_thread,args=(2,partitions[2],queue))
+        t4 = threading.Thread(target=self.signal_computation_thread,args=(3,partitions[3],queue))
+        
+
+        # start threads
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+    
+        # wait until threads finish their job
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
+        
+        for result in self.thread_results:
+            final_audio=final_audio+result
+
         return final_audio
+
+
     def on_click_play(self, button):
         final_audio=self.gen_wav_binary()
                     #print(final_audio[t])
@@ -167,9 +192,12 @@ class GridWindow(Gtk.Window):
     def on_click_export(self, button):
         final_audio=self.gen_wav_binary()
         write("tmp.wav", self.sample_rate, np.array(final_audio).astype(np.int16))
-        with open('tmp.wav', 'rb') as f:
-            r = requests.post('http://10.150.152.83/v1/storage/buckets/636f70ff960601600409/files', files={'tmp.wav': f}, headers= {'X-Appwrite-Project': '636f70dd0ab3fd35c183'})
+        client = Client()
+        client.set_endpoint('http://10.150.152.83/80/v1').set_project('636f70dd0ab3fd35c183')
+        storage = Storage(client)
+        result = storage.create_file('636f70ff960601600409', 'sdfiosjdf', InputFile.from_path('tmp.wav'))
         print("Done")
+        
     def on_click_add_column_button(self, button):
         self.num_columns+=1
         for i in range(len(self.music_matrix)):
