af2534ecf07a67732c276ba4b138bc6a08a672af
Abir Haque <abir.haque.usa@gmail.com>
2022-11-13 03:40:03 -0600

    Reorganized UI and fixed uneven data partitioning

--- daw/daw.py
+++ daw/daw.py
@@ -4,30 +4,34 @@ import numpy as np
 import simpleaudio as sa
 from scipy.io.wavfile import write
 import threading
-import requests
 from appwrite.client import Client
 from appwrite.input_file import InputFile
 from appwrite.services.storage import Storage
 
+from matplotlib.backends.backend_gtk3agg import  FigureCanvasGTK3Agg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
+
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk, Gdk
+from gi.repository import Gtk
 
 
 class GridWindow(Gtk.Window):
     def __init__(self):
 
-        super().__init__(title="Grid Example")
+        super().__init__(title="MuseEQ Studio")
+        sw = Gtk.ScrolledWindow()
 
         self.master_grid = Gtk.Grid()
         self.toolbar_grid= Gtk.Grid()
         self.compose_grid=Gtk.Grid()
         self.pitch_grid=Gtk.Grid()
         self.music_grid=Gtk.Grid()
-        self.set_default_size(200, 200)
+        self.set_default_size(1500, 1200)
         #self.music_grid.size_allocate(Gdk.Rectangle(0,0,2,2))
-
-        self.num_pitches=13
-        self.num_columns=4
+        
+        
+        self.num_columns=8
         self.sample_rate=44100
         self.samples_per_note=11025#44100/4
         self.note_frequency_mapping=[
@@ -43,10 +47,13 @@ class GridWindow(Gtk.Window):
             ('A4','440.00'),
             ('A#4','466.16'),
             ('B4','493.88'),
-            ('C5','523.25'),
+            ('C5','523.25')
         ]
         self.note_frequency_mapping.reverse()
-        self.thread_results=[0,0,0,0]
+        self.num_pitches=len(self.note_frequency_mapping)
+
+        self.max_threads=8
+        self.thread_results=[]
 
 
 
@@ -112,12 +119,34 @@ class GridWindow(Gtk.Window):
 
         self.compose_grid.add(self.pitch_grid)
         self.compose_grid.attach_next_to(self.music_grid, self.pitch_grid, Gtk.PositionType.RIGHT, 1, 1)
-        
+    
 
+
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot()
+
+        self.canvas = FigureCanvas(self.fig)  # a Gtk.DrawingArea
+        self.canvas.set_size_request(600, 400)
 
         self.master_grid.add(self.toolbar_grid)
-        self.master_grid.attach_next_to(self.compose_grid, self.toolbar_grid, Gtk.PositionType.BOTTOM, 1, 1)
-        self.add(self.master_grid)
+        self.master_grid.attach_next_to(self.canvas, self.toolbar_grid, Gtk.PositionType.BOTTOM, 1, 1)
+        self.master_grid.attach_next_to(self.compose_grid, self.canvas, Gtk.PositionType.BOTTOM, 1, 1)
+        self.canvas.show()
+        #self.add(self.master_grid)
+
+        
+        sw.add(self.master_grid)
+        self.add(sw)
+
+
+
+
+
+
+
+
+
+
     def is_float(self, val):
         try:
             val=float(val)
@@ -132,7 +161,7 @@ class GridWindow(Gtk.Window):
                 for t in range(0+j*self.samples_per_note,self.samples_per_note+j*self.samples_per_note):
                     tmp=[token if token!="f" else self.note_frequency_mapping[i][1] for token in queue]#self.note_frequency_mapping[i][1] for token in queue]
                     tmp=[(token if token!='t' else str(t)) for token in tmp]
-                    tmp=[(token if token!="a" else str(buf[i][j])) for token in tmp]                    
+                    tmp=[(token if token!="a" else str(buf[i][j])) for token in tmp]
                     final_audio[t]+=parser.calculate(tmp)
         self.thread_results[id]= final_audio
 
@@ -153,24 +182,25 @@ class GridWindow(Gtk.Window):
             for j in range(len(self.music_matrix[i])):
                 buf[i][j]=0 if self.music_matrix[i][j].get_text()=="" else float(self.music_matrix[i][j].get_text())
         queue = parser.shunting_yard(equation)
-        partitions=np.hsplit(buf, 4)
-        t1 = threading.Thread(target=self.signal_computation_thread,args=(0,partitions[0],queue))
-        t2 = threading.Thread(target=self.signal_computation_thread,args=(1,partitions[1],queue))
-        t3 = threading.Thread(target=self.signal_computation_thread,args=(2,partitions[2],queue))
-        t4 = threading.Thread(target=self.signal_computation_thread,args=(3,partitions[3],queue))
+        threads=[]
+        usuable_threads=1
+        if num_notes>=self.max_threads:
+            usuable_threads=self.max_threads
+        else:
+            usuable_threads=self.num_columns
+        self.thread_results=[0 for x in range(usuable_threads)]
+        partitions=np.hsplit(buf, usuable_threads)
+        for i in range(usuable_threads):
+            threads.append(threading.Thread(target=self.signal_computation_thread,args=(i,partitions[i],queue)))
         
 
         # start threads
-        t1.start()
-        t2.start()
-        t3.start()
-        t4.start()
+        for i in range(usuable_threads):
+            threads[i].start()
     
         # wait until threads finish their job
-        t1.join()
-        t2.join()
-        t3.join()
-        t4.join()
+        for i in range(usuable_threads):
+            threads[i].join()
         
         for result in self.thread_results:
             final_audio=final_audio+result
@@ -199,25 +229,55 @@ class GridWindow(Gtk.Window):
         print("Done")
         
     def on_click_add_column_button(self, button):
-        self.num_columns+=1
-        for i in range(len(self.music_matrix)):
-            self.music_matrix[i].append(Gtk.Entry())
-            self.music_grid.attach_next_to(self.music_matrix[i][len(self.music_matrix[i])-1], self.music_matrix[i][len(self.music_matrix[i])-2], Gtk.PositionType.RIGHT, 1, 1)
-            self.music_matrix[i][len(self.music_matrix[i])-1].show()
+        self.num_columns+=8
+        for k in range(8):
+            for i in range(len(self.music_matrix)):
+                self.music_matrix[i].append(Gtk.Entry())
+                self.music_grid.attach_next_to(self.music_matrix[i][len(self.music_matrix[i])-1], self.music_matrix[i][len(self.music_matrix[i])-2], Gtk.PositionType.RIGHT, 1, 1)
+                self.music_matrix[i][len(self.music_matrix[i])-1].show()
 
     def on_click_remove_column_button(self, button):
-        if self.num_columns>1:
-            for i in range(len(self.music_matrix)):
-                self.music_grid.remove(self.music_matrix[i][self.num_columns-1])
-                self.music_matrix[i].pop()
-            self.num_columns-=1
+        if self.num_columns>8:
+            for k in range(8):
+                for i in range(len(self.music_matrix)):
+                    self.music_grid.remove(self.music_matrix[i][self.num_columns-1])
+                    self.music_matrix[i].pop()
+                self.num_columns-=1
             #self.music_grid.remove_column(self.num_columns)
 
 
 
     def on_click_add_instrument_button(self, button):
         print(self.instrument_combo.get_child().get_text())
-        self.instrument_store.append([self.instrument_combo.get_child().get_text()])
+        self.instrument_store.append([self.instrument_combo.get_child().get_text()]) 
+        parser=Parser()
+        queue = parser.shunting_yard(self.instrument_combo.get_child().get_text())
+        t_indices=[]
+        for i in range(len(queue)):
+            if queue[i]=="a":
+                queue[i]="1"
+            elif queue[i]=="f":
+                queue[i]="440.0"
+            elif queue[i]=="t":
+                t_indices.append(i)
+        data=[]
+        for t in range(0,44100,441):
+            for k in t_indices:
+                queue[k]=str(float(t))
+            data.append(parser.calculate(queue))
+        
+
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot()
+        self.ax.plot(np.arange(0.0,44100,441),data)
+
+        self.canvas = FigureCanvas(self.fig)  # a Gtk.DrawingArea
+        self.canvas.set_size_request(600, 400)
+        self.master_grid.attach_next_to(self.canvas, self.toolbar_grid, Gtk.PositionType.BOTTOM, 1, 1)
+        self.master_grid.attach_next_to(self.compose_grid, self.canvas, Gtk.PositionType.BOTTOM, 1, 1)
+        self.canvas.show()
+        
+
     def on_click_remove_instrument_button(self, button):
         self.instrument_store.remove(self.instrument_store[-1].iter)
 
@@ -229,5 +289,6 @@ class GridWindow(Gtk.Window):
 
 win = GridWindow()
 win.connect("destroy", Gtk.main_quit)
+
 win.show_all()
 Gtk.main()
